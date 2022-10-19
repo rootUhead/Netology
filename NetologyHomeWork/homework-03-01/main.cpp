c10f07ef7603b4dbbec3514e6d3f41779faabd81
Ilya Minin <106930314+rootUhead@users.noreply.github.com>
2022-10-19 23:08:41 +0300

    add in private num1 and num2

--- NetologyHomeWork/homework-03-01/main.cpp
+++ NetologyHomeWork/homework-03-01/main.cpp
@@ -9,40 +9,43 @@
 
 class Calculator
 {
-public:
+private:
 	double num1;
 	double num2;
+
+public:
 	
-	double add(double &num1, double &num2){
-		return num1+num2;
+	double add(){
+		return num1 + num2;
 	}
-	double multiply(double &num1, double &num2){
+	
+	double multiply(){
 		return num1*num2;
 	}
-	double subtract_1_2(double &num1, double &num2){
+	double subtract_1_2(){
 		return num1-num2;
 	}
-	double subtract_2_1(double &num1, double &num2){
+	double subtract_2_1(){
 		return num2-num1;
 	}
-	double divide_1_2(double &num1, double &num2){
+	double divide_1_2(){
 		return num1/num2;
 	}
-	double divide_2_1(double &num1, double &num2){
+	double divide_2_1(){
 		return num2/num1;
 	}
-	bool set_num1(double num1){
+	bool set_num1(double a){
 		if (num1 !=0){
-			this -> num1 = num1;
+			num1 = a;
 			return true;
 		}
 		else
 			return false;
 	}
 	
-	bool set_num2(double num2){
+	bool set_num2(double a){
 		if (num2 !=0){
-			this -> num2 = num2;
+			num2 = a;
 			return true;
 		}
 		else
@@ -53,25 +56,26 @@ public:
 int main(int argc, const char * argv[]) {
 
 	Calculator calc;
+	double a;
 	do{
 		std::cout << "Введите num1: ";
-		std::cin >> calc.num1;
-		if (!calc.set_num1(calc.num1))
+		std::cin >> a;
+		if (!calc.set_num1(a))
 			std::cout << "Неверный ввод!" << std::endl;
-	} while (!calc.set_num1(calc.num1));
+	} while (!calc.set_num1(a));
 	
 	do{
 		std::cout << "Введите num2: ";
-		std::cin >> calc.num2;
-		if (!calc.set_num2(calc.num2))
+		std::cin >> a;
+		if (!calc.set_num2(a))
 			std::cout << "Неверный ввод!" << std::endl;
-	} while (!calc.set_num2(calc.num2));
+	} while (!calc.set_num2(a));
 	
-	std::cout << "num1 + num2 = " << calc.add(calc.num1, calc.num2)<< std::endl;
-	std::cout << "num1 - num2 = " << calc.subtract_1_2(calc.num1, calc.num2)<< std::endl;
-	std::cout << "num2 - num1 = " << calc.subtract_2_1(calc.num1, calc.num2)<< std::endl;
-	std::cout << "num1 * num2 = " << calc.multiply(calc.num1, calc.num2)<< std::endl;
-	std::cout << "num1 / num2 = " << calc.divide_1_2(calc.num1, calc.num2)<< std::endl;
-	std::cout << "num2 / num1 = " << calc.divide_2_1(calc.num1, calc.num2)<< std::endl;
+	std::cout << "num1 + num2 = " << calc.add()<< std::endl;
+	std::cout << "num1 - num2 = " << calc.subtract_1_2()<< std::endl;
+	std::cout << "num2 - num1 = " << calc.subtract_2_1()<< std::endl;
+	std::cout << "num1 * num2 = " << calc.multiply()<< std::endl;
+	std::cout << "num1 / num2 = " << calc.divide_1_2()<< std::endl;
+	std::cout << "num2 / num1 = " << calc.divide_2_1()<< std::endl;
 	
 }
